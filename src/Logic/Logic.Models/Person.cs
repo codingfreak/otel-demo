dccdd996addd0da6ba4b61cbad1564e0b0d9c839
Alexander Schmidt <alexander.schmidt@devdeer.com>
2026-03-02 10:49:20 +0100

    Project initialized

---
+++ src/Logic/Logic.Models/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace codingfreaks.OtelDemo.Logic.Models
+{
+    public class Person
+    {
+        #region properties
+
+        public string Firstname { get; set; } = null!;
+
+        public string Lastname { get; set; } = null!;
+
+        #endregion
+    }
+}
---
+++ src/Services/Services.CoreApi/Controllers/PeopleController.cs
@@ -0,0 +1,23 @@
+﻿namespace codingfreaks.OtelDemo.Services.CoreApi.Controllers
+{
+    using Logic.Models;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PeopleController : ControllerBase
+    {
+        #region methods
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAsync()
+        {
+            var result = Array.Empty<Person>();
+            await Task.Yield();
+            return Ok(result);
+        }
+
+        #endregion
+    }
+}
